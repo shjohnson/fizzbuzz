a2a23c59e13c23e257eb05cff44c2a5ee877d1d8
Sarah Pinder <r.s.pinder@gmail.com>
2013-09-22 13:54:39 +0100

    Say 'fizz' instead of any number divisible by 3.

--- README.md
+++ README.md
@@ -7,3 +7,11 @@ How to use
 ==========
 
 Run this from your command line using `ruby fizzbuzz.rb`.
+
+
+About
+=====
+
+This is a somewhat unusual implementation of the standard FizzBuzz, as spec'ed by [Blackboard](http://blackboard-projects.herokuapp.com/projects/1).
+
+It uses explicit division & inspection of the remainder to work out if a number is divisible by another. Using `.remainder` would be far more efficient, but this is intended to demonstrate that there are multiple solutions to problems, some of which are quicker or more readable than others.
--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,30 @@
 RANGE = 1..100
 
+# First, converts the number to a float, then divides it by 3.0.
+# If the result is a whole number (e.g. 3.0 / 3.0 #=> 1.0), then it is divisible.
+# If the number is not whole (e.g. 7.0 / 3.0 #=> 2.3333....) then it is not.
+# Returns true or false.
+def divisible_by_three?(number)
+  result = (number.to_f / 3.0)
+  represents_a_whole_number?(result)
+end
+
+# Is this float a whole number (e.g. is it 3.0, or 3.12?)
+# .to_i rounds a float down to an integer.
+# Comparing an integer to a float using == will return true
+# if they represent the same number, (e.g. 3.0 == 3 #=> true).
+# Returns true or false.
+def represents_a_whole_number?(float)
+  float.to_i == float
+end
+
 RANGE.each do |number|
-  puts number
+
+  if divisible_by_three?(number)
+    output = "fizz"
+  else
+    output = number.to_s
+  end
+
+  puts output
 end
